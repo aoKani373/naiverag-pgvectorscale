004dfe8db4cfed4704136e7acf30a095aa226fa0
aoKani373 <ask_o373@outlook.com>
2025-12-24 15:01:36 +0000

    create table models as placeholder

--- naiverag_server/main.py
+++ naiverag_server/main.py
@@ -1,3 +1,39 @@
+from sqlmodel import SQLModel, Field, create_engine
+
+
+class AuthorBase(SQLModel):
+    pass
+
+class Author(AuthorBase, table=True):
+    id: int | None = Field(default=None)
+
+
+class NovelBase(SQLModel):
+    pass
+
+class Novel(NovelBase, table=True):
+    id: int | None = Field(default=None)
+
+
+class ChapterBase(SQLModel):
+    pass
+
+class Chapter(ChapterBase, table=True):
+    id: int | None = Field(default=None)
+
+class EpisodeBase(SQLModel):
+    pass
+
+class Episode(EpisodeBase, table=True):
+    id: int | None = Field(default=None)
+
+
+database_url = ""
+engine = create_engine(database_url, echo=True)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
 def main():
     print("Hello from naiverag-pgvectorscale!")
 
